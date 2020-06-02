91f7e8e4e19e09eb8a96e5131006f5a82db6c2ea
caseychester7414 <casey.chester@bryanuniversity.edu>
2020-06-02 09:14:39 -0500

    todo

---
+++ week1/todo/app.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function App() {
+    return (
+        <div>
+            <input type="checkbox" />
+            <p> place holder</p>
+            <input type="checkbox" />
+            <p> place holder</p>
+            <input type="checkbox" />
+            <p> place holder</p>
+            <input type="checkbox" />
+            <p> place holder</p>
+        </div>
+    )
+}
+
+export default App
