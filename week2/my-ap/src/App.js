f4204c8676db460e45b3c84c45e8b800519e60b3
caseychester7414 <casey.chester@bryanuniversity.edu>
2020-06-09 13:19:04 -0500

    assign2

---
+++ week2/my-ap/src/App.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import Location from './location';
+import './App.css';
+let vacationSpots = [
+  
+
+  {
+
+    place: "Meridian, Idaho",
+
+    price: 40,
+
+    timeToGo: "Spring"
+
+  },{
+
+    place: "Cancun",
+
+    price: 900,
+
+    timeToGo: "Winter"
+
+  },{
+
+    place: "China",
+
+    price: 1200,
+
+    timeToGo: "Fall"
+
+  },{
+
+    place: "Russia",
+
+    price: 1100,
+
+    timeToGo: "Summer"
+
+  },{
+
+    place: "Lebanon",
+
+    price: 400,
+
+    timeToGo: "Spring"
+
+  }
+
+]
+console.log(vacationSpots)
+
+function App(){
+return (
+  <div>
+    {vacationSpots.map(location => {
+  return(
+      <Location 
+          place={location.place} 
+          price={location.price} 
+          timeToGo={location.timeToGo} 
+      />
+  )
+})}
+  </div>
+)
+}
+
+
+
+export default App
---
+++ week2/my-ap/src/location.js
@@ -0,0 +1,14 @@
+import React from 'react';
+function Location(props){
+    return (
+      <div>
+        {props.place}
+        {props.timeToGo}
+        {props.price}
+        
+        
+      </div>
+    )
+    }
+    export default Location
+    
