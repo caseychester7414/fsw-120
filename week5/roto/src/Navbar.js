5cbbe3b96f0cf23d7113a5d217d7be584eafa20c
caseychester7414 <casey.chester@bryanuniversity.edu>
2020-07-12 00:58:16 -0500

    calculator

--- week5/roto/src/Navbar.js
+++ week5/roto/src/Navbar.js
@@ -4,9 +4,15 @@ import './Navbar.css'
 function Navbar() {
     return (
         <div className='navbarWrapper'>
+            <div classname="div1">
             <Link className='links' to='/'>HOME</Link>
+            </div>
+            <div classname="div2">
             <Link className='links' to='/about'>ABOUT</Link>
+            </div>
+            <div classname="div3">
             <Link className='links' to='/service'>SERVICES</Link>
+            </div>
         </div>
     )
 }
